faab70a894e18b5eb30e205993850cc8da31f6cb
debora-rubio <debora.rubio@fatec.sp.gov.br>
2026-05-09 22:33:59 -0300

    nodo_lista.py
    criar classe nodolista - nó de uma lista encadeada.

---
+++ nodo_lista.py
@@ -0,0 +1,15 @@
+# Classe NodoLista - Nó de uma lista encadeada
+# Guarda uma música e a referência para o próximo nó
+
+from musica import Musica
+
+
+class NodoLista:
+    # Construtor - recebe uma música e armazena
+    def __init__(self, musica):
+        self.musica = musica   # A música que este nó guarda
+        self.proximo = None    # Referência para o próximo nó (vazio no começo)
+    
+    # Método para mostrar o nó
+    def __str__(self):
+        return str(self.musica)
