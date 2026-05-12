15ab1db85a36a464eeea6f16ecf59f732d9bef5f
debora-rubio <debora.rubio@fatec.sp.gov.br>
2026-05-11 21:43:31 -0300

    musica.py
    inclui print do id da musica quando solicitar a listagem da biblioteca no menu.

--- musica.py
+++ musica.py
@@ -1,14 +1,11 @@
-# Classe Musica - Armazena os dados de uma música
-
 class Musica:
-    # Construtor - recebe os dados da música e armazena
     def __init__(self, id, titulo, artista, genero, bpm):
-        self.id = id              # Número único da música
-        self.titulo = titulo      # Nome da música
-        self.artista = artista    # Quem canta
-        self.genero = genero      # Tipo de música
-        self.bpm = bpm            # Velocidade da música
-    
-    # Método para mostrar a música
-    def __str__(self):
-        return self.titulo + " - " + self.artista
+        self.id = id
+        self.titulo = titulo
+        self.artista = artista
+        self.genero = genero
+        self.bpm = bpm
+
+    def __str__(self):        # Exibe o ID antes dos dados da música.
+        
+        return f"ID: {self.id} | {self.titulo} - {self.artista} ({self.bpm} BPM)"
